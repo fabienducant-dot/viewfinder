d0068e60870c6be9530b5af453ea03fa54a9b038
fabienducant-dot <fabien.ducant@gmail.com>
2026-07-26 19:02:04 +0200

    Renouveler le cache PWA

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    au lieu de rester bloqué sur une version mise en cache. Seuls les fichiers statiques qui ne
    changent presque jamais (icônes) utilisent une stratégie cache-d'abord. */
 
-const CACHE_NAME = "viewfinder-cache-v15";
+const CACHE_NAME = "viewfinder-cache-v16";
 const APP_SHELL = [
   "./",
   "./index.html",
